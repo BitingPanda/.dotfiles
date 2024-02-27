7f44a81716dbf17ffd05f836a459613428eaa627
Pranesh <70361486+BitingPanda@users.noreply.github.com>
2024-02-27 17:42:40 +0530

    Added LSP Support

---
+++ init.lua
@@ -0,0 +1,19 @@
+vim.g.airline_powerline_fonts = 1
+require("neovim-config")
+
+--For debugging
+	--print("hello")
+	
+--For calling LuaLine
+require('lualine').setup()
+
+-- Enable line numbering
+vim.opt.nu = true
+
+-- Relative Line number
+vim.opt.relativenumber = true
+
+--colorscheme // Disabled for now
+vim.cmd("colorscheme kanagawa")
+vim.cmd("lua colorMyPencils()")
+
